579ea4df445ab5b328b316eaf0476f993dbbc539
Oterem <ot1234ster@gmail.com>
2017-10-13 14:30:48 +0300

    -Glide image library for loading large images was added. - Sampling two points: one for blob and  other from skin

--- app/src/main/java/omri/opencvdemo/MainActivity.java
+++ app/src/main/java/omri/opencvdemo/MainActivity.java
@@ -4,10 +4,19 @@ package omri.opencvdemo;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import android.graphics.Bitmap;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -18,8 +27,10 @@ import android.provider.MediaStore;
 
 import android.support.v4.content.FileProvider;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -52,17 +63,24 @@ import org.opencv.imgproc.Moments;
 import org.opencv.photo.Photo;
 import org.opencv.android.OpenCVLoader;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
+import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Vector;
 
+import com.bumptech.glide.Glide;
 import com.theartofdev.edmodo.cropper.CropImage;
+
+import static org.opencv.imgproc.Imgproc.COLOR_BGR2GRAY;
+import static org.opencv.imgproc.Imgproc.FLOODFILL_MASK_ONLY;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -73,11 +91,15 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private String currentPhotoPath, currentGalleryPath;
     private String current_open_image_path;
-    static final int ACTION_IMAGE_CAPTURE = 1;
-    static final int ACTION_GET_CONTENT = 2;
+    private static final int ACTION_IMAGE_CAPTURE = 1;
+    private static final int ACTION_GET_CONTENT = 2;
     private static final int REQUEST_CAMERA = 100;
+    private static int STATE = 3;
+    private static final int SAMPLE_BLOB = 3;
+    private static final int SAMPLE_SKIN = 4;
     private Uri photoURI;
     private ProgressBar pb;
+    private Point seed, skin;
 
 
     // Used to load the 'native-lib' library on application startup.
@@ -180,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
                         startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
                         Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), photoURI);
                         currentBitmap = bm;
-                        setPic(bm);
+                        setPic(bm, photoURI);
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "Error taking picture", Toast.LENGTH_LONG).show();
                     }
@@ -191,10 +213,10 @@ public class MainActivity extends AppCompatActivity {
                         Uri receivedUri = data.getData();
                         photoURI = receivedUri;
                         Intent intent = CropImage.activity(photoURI).getIntent(getBaseContext());
-                        startActivityForResult(intent,CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
-                        Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), receivedUri);
+                        startActivityForResult(intent, CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+                       /* Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), receivedUri);
                         currentBitmap = bm;
-                        setPic(bm);
+                        setPic(bm, receivedUri);*/
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "Error loading picture", Toast.LENGTH_LONG).show();
                     }
@@ -203,9 +225,13 @@ public class MainActivity extends AppCompatActivity {
                     CropImage.ActivityResult result = CropImage.getActivityResult(data);
                     Uri resultUri = result.getUri();
                     try {
+
                         Bitmap bm = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
                         currentBitmap = bm;
-                        setPic(bm);
+                        //getBlobCoordinates();
+                        setPic(bm, resultUri);
+
+
                     } catch (Exception e) {
                         Toast.makeText(getApplicationContext(), "Error cropping picture", Toast.LENGTH_LONG).show();
                     }
@@ -217,9 +243,143 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*----------------------------------------------------------------------------*/
-    private void setPic(Bitmap bm) {
+    private void getBlobCoordinates() {
+        seed = new Point();
+
+
+        final AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Click on suspicions blob");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
+        mImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+
+                if (STATE == SAMPLE_SKIN) {
+                    skin = new Point();
+                    int[] locations = new int[2];
+                    view.getLocationOnScreen(locations);
+                    Log.i(TAG, "onTouch: locations are:" + locations[0] + ", " + locations[1]);
+
+
+                    skin.x = (int) motionEvent.getRawX();
+                    skin.y = (int) motionEvent.getRawY();
+
+                    mImageView.setOnTouchListener(null);
+                    STATE=SAMPLE_BLOB;
+
+
+                    Log.i(TAG, "onTouch: seed is:" + seed.x + ", " + seed.y);
+                    Log.i(TAG, "onTouch: skin is:" + skin.x + ", " + skin.y);
+                   // drawCircle();
+
+
+                    MyAsyncTask work = new MyAsyncTask();
+                    calculatedBitmap = currentBitmap;
+                    work.execute(calculatedBitmap);
+                    ImageButton b = (ImageButton) findViewById(R.id.analyze_btn);
+                    b.setEnabled(false);
+                    return false;
+
+                }
+
+                if (STATE == SAMPLE_BLOB) {
+                    seed.x = (int) motionEvent.getRawX();
+                    seed.y = (int) motionEvent.getRawY();
+
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("Click on the skin");
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                    STATE = SAMPLE_SKIN;
+                    return false;
+                }
+
+
+
+
+
+
+             /*   MyAsyncTask work = new MyAsyncTask();
+                calculatedBitmap = currentBitmap;
+                work.execute(calculatedBitmap);
+                ImageButton b = (ImageButton) findViewById(R.id.analyze_btn);
+                b.setEnabled(false);*/
+                return false;
+
+            }
+        });
+
+
+    }
+
+
+    private void drawCircle() {
+        Bitmap bitmap = Bitmap.createBitmap(currentBitmap.getWidth(),currentBitmap.getHeight(), Bitmap.Config.RGB_565);
+
+        //bitmap.setPixel(mImageView.getWidth(), mImageView.getHeight(), 0);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawBitmap(bitmap, 0, 0, null);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawCircle((float)seed.x,(float)seed.y,10,paint);
+        mImageView.setImageDrawable(new BitmapDrawable(getBaseContext().getResources(),bitmap)) ;
+
+
+    }
+    /*-------------------------------------------------------------------*/
+    public void getSkinCoordinates() {
+        skin = new Point();
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Alert");
+        alertDialog.setMessage("Please click on skin in picture, but not the blob");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        mImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                skin.x = (int) motionEvent.getRawX();
+                skin.y = (int) motionEvent.getRawY();
+
+
+                Log.i(TAG, "onTouch: skin is:" + skin.x + ", " + skin.y);
+              //  mImageView.setImageBitmap(drawCircle());
+                return true;
+            }
+        });
+
+        // mImageView.setOnTouchListener(null);
+    }
+
+
+    /*----------------------------------------------------------------------------*/
+    private void setPic(Bitmap bm, Uri resultUri) {
+
+
         mImageView = (ImageView) findViewById(R.id.pic1);
-        mImageView.setImageBitmap(bm);
+
+        Glide
+                .with(this)
+                .asBitmap().load(resultUri).into(mImageView);
+
         analyze_btn.setEnabled(true);
     }
 
@@ -236,11 +396,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void onAnalyzeClick(View v) {
 
-        MyAsyncTask work = new MyAsyncTask();
-        calculatedBitmap = currentBitmap;
-        work.execute(calculatedBitmap);
-        ImageButton b = (ImageButton) findViewById(R.id.analyze_btn);
-        b.setEnabled(false);
+        //insert here info dialog fro user
+        getBlobCoordinates();
+
+
     }
     /*----------------------------------------------------------------------------*/
 
@@ -284,6 +443,14 @@ public class MainActivity extends AppCompatActivity {
             View v = findViewById(R.id.my_layout);
             v.setAlpha(1f);
             calculatedBitmap = bitmap;
+
+
+            BitmapFactory.Options myOptions = new BitmapFactory.Options();
+
+            myOptions.inScaled = false;
+            myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+
+
             mImageView.setImageBitmap(calculatedBitmap);
         }
 
@@ -293,33 +460,66 @@ public class MainActivity extends AppCompatActivity {
         protected Bitmap doInBackground(Bitmap... bitmaps) {
 
             bm = bitmaps[0];
-            Mat src = new Mat();
+            Mat src = new Mat(bm.getHeight(), bm.getWidth(), CvType.CV_8SC3);
+
+
             Mat dest = new Mat();
 
-            Utils.bitmapToMat(bm, src);
+          //  Bitmap resultBitmap = Bitmap.createBitmap(bm, 0, 0, bm.getWidth() - 1, bm.getHeight() - 1);
+           // Utils.bitmapToMat(resultBitmap, src);
+          /*  Mat mask = new Mat(dest.cols()+2,dest.rows()+2,CvType.CV_8UC1);
+            Color blob = new Color();
+            int pixel = bm.getPixel((int)seed.x,(int)seed.y);
+            Vector<Integer> redChannel =  new Vector<>();
+            redChannel=  avarageColorChannel(seed,1);
+            Vector<Integer> blueChannel = new Vector<>();
+            blueChannel=     avarageColorChannel(seed,2);
+            Vector<Integer> greenChannel =  new Vector<>();
+            greenChannel=  avarageColorChannel(seed,3);
 
+            int avarageRed = avarageValue(redChannel);
+            int avarageBlue = avarageValue(blueChannel);
+            int avaragegreen = avarageValue(greenChannel);
+
+            int redValue = Color.red(pixel);
+            int blueValue = Color.blue(pixel);
+            int greenValue = Color.green(pixel);
+
+            int redValueSkin = 164;
+            int blueValueSkin = 126;
+            int greenValueSkin = 134;*/
+
+            //Log.i(TAG, "doInBackground: red: "+ redValue+" , blue: "+ blueValue+" , green: "+greenValue);
+
+            // Imgproc.floodFill(src,mask,seed,new Scalar(255,255,255),null,)
+
+
+            // Imgproc.cvtColor(src, dest, Imgproc.Co);
+         /*   int pixel = resultBitmap.getPixel((int) seed.x, (int) seed.y);
+
+            int blueValue = Color.blue(pixel);
+            int greenValue = Color.green(pixel);
+            int redValue = Color.red(pixel);
+            Mat mask = new Mat(src.rows() + 2, src.cols() + 2, CvType.CV_8UC1);
+            Scalar lower = new Scalar(blueValue - 20, greenValue - 20, redValue - 20);
+            Scalar upper = new Scalar(blueValue + 20, greenValue + 20, redValue + 20);
+
+            Imgproc.floodFill(src, mask, seed, new Scalar(0, 0, 0), new Rect(), lower, upper, FLOODFILL_MASK_ONLY);*/
+
+            Utils.bitmapToMat(bm,src);
             Imgproc.cvtColor(src, dest, Imgproc.COLOR_BGR2GRAY);
-
-
-
             Mat kernel = new Mat();
 
-            Imgproc.threshold(dest,dest,0,255,Imgproc.THRESH_BINARY+Imgproc.THRESH_OTSU);
-            Imgproc.morphologyEx(dest,dest,Imgproc.MORPH_OPEN,kernel);
+            Imgproc.threshold(dest, dest, 0, 255, Imgproc.THRESH_BINARY + Imgproc.THRESH_OTSU);
 
+
+            Imgproc.morphologyEx(dest, dest, Imgproc.MORPH_OPEN, kernel);
 
 
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hierarchy = new Mat();//for findContours calculation. Do not touch.
 
             Imgproc.findContours(dest, contours, hierarchy, Imgproc.RETR_CCOMP, Imgproc.CHAIN_APPROX_TC89_KCOS);
-            /*Convert picture back to colors in order to see the red border surrounding the melanoma*/
-           // Imgproc.cvtColor(dest, dest, Imgproc.COLOR_GRAY2RGB);
-            /*Painting red border around the melanoma based on the contour vector*/
-          //  Imgproc.drawContours(dest, contours, -1, new Scalar(255, 0, 0), 10);
-            /*Filling the inside of the contours in white color in order to get rid of "noises" */
-           // Imgproc.drawContours(dest, contours, -1, new Scalar(255, 255, 255), -1);
-
 
 
             List<MatOfPoint> contoursClone = new ArrayList<>();
@@ -329,11 +529,8 @@ public class MainActivity extends AppCompatActivity {
             double area = 0;
 
 
-
-
-
             int num_of_contours = contours.size();
-           while (num_of_contours > 5 & scale <= 14) {
+            while (num_of_contours > 5 & scale <= 14) {
                 contours.clear();
                 contoursClone.clear();
                 scale++;
@@ -352,13 +549,8 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-           /*For printing the contours's area*/
-         /*  for (int i = 0; i < contoursClone.size(); i++) {
 
-                area = Imgproc.contourArea(contoursClone.get(i));
-                Log.i(TAG, "doInBackground: clone " + area);
 
-            }*/
 
 
             Imgproc.erode(cloneDest, cloneDest, new Mat(15, 15, CvType.CV_8U));
@@ -368,7 +560,9 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-            /*For finding centroid*/
+
+
+
             double minimalRadius = 0;
             Imgproc.drawContours(cloneDest, contoursClone, -1, new Scalar(255, 255, 255), -1);
             List<Moments> mu = new ArrayList<Moments>(contours.size());
@@ -381,17 +575,60 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-            Bitmap bm = Bitmap.createBitmap(cloneDest.cols(), cloneDest.rows(), Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(cloneDest, bm);
+            Bitmap bm = Bitmap.createBitmap(dest.cols(), dest.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(dest, bm);
             src.release();
             dest.release();
-            cloneDest.release();
-            hierarchy.release();
-            kernel.release();
+              cloneDest.release();
+             hierarchy.release();
+             kernel.release();
             return bm;
         }
       /*----------------------------------------------------------*/
 
+    }
+
+    private Vector<Integer> avarageColorChannel(Point seed, int flag) {
+        Bitmap bm = currentBitmap;
+        Vector<Integer> vector = new Vector<>();
+        int pixelUp = bm.getPixel((int) seed.x, (int) seed.y - 1);
+        int pixelDown = bm.getPixel((int) seed.x, (int) seed.y + 1);
+        int pixelLeft = bm.getPixel((int) seed.x - 1, (int) seed.y);
+        int pixelRight = bm.getPixel((int) seed.x + 1, (int) seed.y);
+        if (flag == 1) {
+            vector.add(Color.red(pixelUp));
+            vector.add(Color.red(pixelDown));
+            vector.add(Color.red(pixelLeft));
+            vector.add(Color.red(pixelRight));
+            vector.add(Color.red(bm.getPixel((int) seed.x, (int) seed.y)));
+
+        }
+        if (flag == 2) {
+            vector.add(Color.blue(pixelUp));
+            vector.add(Color.blue(pixelDown));
+            vector.add(Color.blue(pixelLeft));
+            vector.add(Color.blue(pixelRight));
+            vector.add(Color.blue(bm.getPixel((int) seed.x, (int) seed.y)));
+
+        }
+        if (flag == 3) {
+            vector.add(Color.green(pixelUp));
+            vector.add(Color.green(pixelDown));
+            vector.add(Color.green(pixelLeft));
+            vector.add(Color.green(pixelRight));
+            vector.add(Color.green(bm.getPixel((int) seed.x, (int) seed.y)));
+
+        }
+
+        return vector;
+    }
+
+    private int avarageValue(Vector<Integer> vector) {
+        int sum = 0;
+        for (int val : vector) {
+            sum += val;
+        }
+        return sum / vector.size();
     }
 
     public void onHistogramClicked(View v) {
